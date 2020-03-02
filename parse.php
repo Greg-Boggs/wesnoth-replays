87087ae2bd1d059f4c141f953c2c67cc957278d5
Greg Boggs <me@gregboggs.com>
2020-03-02 11:40:59 -0800

    fix code indenting and stop at 3 replay files

--- parse.php
+++ parse.php
@@ -1,6 +1,5 @@
 <?php
 
-$directory = "games/";
 $server_message_found = false;
 $loser = '';
 $losers = [];
@@ -11,10 +10,10 @@ $team = [];
 $teams = [];
 $losing_team = '';
 $factions = [];
-
 $filenames = [];
-$dayNum = date('d');
+$i = 0;
 
+$dayNum = date('d');
 $base_url = "http://replays.wesnoth.org/1.14/2020/03/$dayNum/";
 
 $input = @file_get_contents($base_url) or die("Could not access file: $base_url");
@@ -22,87 +21,97 @@ $regexp = "<a href=\"([^\"]*)\">(.*)<\/a>";
 preg_match_all("/$regexp/siU", $input, $matches, PREG_SET_ORDER) or die("Could not find any replay files!");
 
 foreach($matches as $filename) {
-  $info = pathinfo($filename[1]);
-  if (isset($info['extension']) && $info['extension'] == 'bz2') {
 
-      $fh = bzopen($base_url . $filename[1], "r");
+    $info = pathinfo($filename[1]);
+    if (isset($info['extension']) && $info['extension'] == 'bz2') {
+        $i++;
 
-      while ($line = fgets($fh)) {
+        // stop at 4 replay files.
+        if ($i > 3) {
+            exit;
+        }
+        $fh = bzopen($base_url . $filename[1], "r");
+        while ($line = fgets($fh)) {
+            if (strpos($line, 'mp_use_map_settings=no') !== FALSE) {
 
-          // Finished processing player data so clear the player.
-          if (strpos($line, "[/side]") !== FALSE) {
-              $player_id = '';
-              continue;
-          }
+                //Skip games that don't use map settings
+                continue;
+            }
 
-          if (preg_match('/current_player="(.*)"/', $line, $player)) {
-              $player_id = $player[1];
-              continue;
-          }
+            // Finished processing player data so clear the player.
+            if (strpos($line, "[/side]") !== FALSE) {
+                $player_id = '';
+                continue;
+            }
 
-          if ($player_id && preg_match('/team_name="(.*)"/', $line, $team)) {
+            if (preg_match('/current_player="(.*)"/', $line, $player)) {
+                $player_id = $player[1];
+                continue;
+            }
 
-              // Add the player to the teams
-              $teams[$player_id] = $team[1];
-              continue;
-          }
-          if ($player_id && preg_match('/faction="(.*)"/', $line, $faction)) {
-              $factions[$player_id] = $faction[1];
-          }
+            if ($player_id && preg_match('/team_name="(.*)"/', $line, $team)) {
 
-          // If the last line was a server message, process this one.
-          if ($server_message_found) {
+                // Add the player to the teams
+                $teams[$player_id] = $team[1];
+                continue;
+            }
+            if ($player_id && preg_match('/faction="(.*)"/', $line, $faction)) {
+                $factions[$player_id] = $faction[1];
+            }
 
-              // Check for rejoin and remove loser
-              if (strpos($line, "$loser has joined") !== FALSE) {
-                  $loser = '';
-              }
+            // If the last line was a server message, process this one.
+            if ($server_message_found) {
 
-              if (preg_match('/"(.*) has left the game/', $line, $player)) {
-                  print($player[1] . " has left!\n");
+                // Check for rejoin and remove loser
+                if (strpos($line, "$loser has joined") !== FALSE) {
+                    $loser = '';
+                }
 
-                  // Are you the first person to leave?
-                  if (!$loser) {
-                      $loser = $player[1];
-                  } else {
+                if (preg_match('/"(.*) has left the game/', $line, $player)) {
+                    print($player[1] . " has left!\n");
 
-                      // Two people have now left. So, the game is over.
-                      break;
-                  }
-              }
-              if (preg_match('/"(.*) has surrendered/', $line, $player)) {
-                  $loser = $player[1];
+                    // Are you the first person to leave?
+                    if (!$loser) {
+                        $loser = $player[1];
+                    } else {
 
-                  // Game over as soon as one player on a side surrenders
-                  break;
-              }
+                        // Two people have now left. So, the game is over.
+                        break;
+                    }
+                }
+                if (preg_match('/"(.*) has surrendered/', $line, $player)) {
+                    $loser = $player[1];
 
-              // Reset server message flag
-              $server_message_found = false;
-          }
+                    // Game over as soon as one player on a side surrenders
+                    break;
+                }
 
-          // If it's a server message!
-          if (strpos($line, 'id="server"') !== FALSE) {
-              $server_message_found = true;
-          }
-      }
+                // Reset server message flag
+                $server_message_found = false;
+            }
 
-      if ($loser) {
-          $losing_team = $teams[$loser];
-          $losers[] = $loser;
-      }
+            // If it's a server message!
+            if (strpos($line, 'id="server"') !== FALSE) {
+                $server_message_found = true;
+            }
+        }
 
-      foreach ($teams as $player => $team) {
-          if ($team == $losing_team) {
-              $losers[] = $player;
-              echo "$player lost as $factions[$player]!\n";
-          } else {
-              $winners[] = $player;
-              echo "$player as $factions[$player]!\n";
-          }
-      }
+        if ($loser) {
+            $losing_team = $teams[$loser];
+            $losers[] = $loser;
+        }
+
+        foreach ($teams as $player => $team) {
+            if ($team == $losing_team) {
+                $losers[] = $player;
+                echo "$player lost as $factions[$player]!\n";
+            } else {
+                $winners[] = $player;
+                echo "$player as $factions[$player]!\n";
+            }
+        }
 
 
-      bzclose($fh);
-  }
+        bzclose($fh);
+    }
 }
